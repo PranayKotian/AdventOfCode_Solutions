88cd98bcb1d1ddc700a89bdd43481cc171ca26d2
kotianp <kotianp@mcmaster.ca>
2023-12-06 19:34:53 -0500

    updated folders and day1part2 solution

---
+++ 2023_python_solutions/Day1/day1part2.py
@@ -0,0 +1,57 @@
+#Advent of Code 2023: Day 1 Part 2 solution
+#Author: Pranay Kotian
+
+#Input text not modified
+with open("Day1/text1.txt") as file:
+    lines = file.readlines()
+
+numbers = {
+  'one': "1",
+  'two': "2",
+  'three': "3",
+  'four': "4",
+  'five': "5",
+  'six': "6",
+  'seven': "7",
+  'eight': "8",
+  'nine': "9"
+}
+numbersrev = {k[::-1]:v for k,v in numbers.items()}
+
+total = 0
+
+#inputs are assumed to eventually have a numeric or text-based number
+#otherwise, both loops would run into indexOutOfRange errors
+for line in lines:
+  for idx,i in enumerate(line):
+    if i.isnumeric():
+      first = i 
+      break
+    if line[idx:idx+3] in numbers:
+      first = numbers[line[idx:idx+3]]
+      break
+    if line[idx:idx+4] in numbers:
+      first = numbers[line[idx:idx+4]]
+      break
+    if line[idx:idx+5] in numbers:
+      first = numbers[line[idx:idx+5]]
+      break
+  nameRev = line[::-1]
+
+  for idx,j in enumerate(nameRev):
+    if j.isnumeric():
+      last = j
+      break
+    if nameRev[idx:idx+3] in numbersrev:
+      last = numbersrev[nameRev[idx:idx+3]]
+      break
+    if nameRev[idx:idx+4] in numbersrev:
+      last = numbersrev[nameRev[idx:idx+4]]
+      break
+    if nameRev[idx:idx+5] in numbersrev:
+      last = numbersrev[nameRev[idx:idx+5]]
+      break
+  
+  total += int(first+last)
+
+print(total)
