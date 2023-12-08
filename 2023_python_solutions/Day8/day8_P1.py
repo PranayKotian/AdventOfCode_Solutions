a0f5960846817e3efe8c850959e8a6eac33bb8fa
kotianp <kotianp@mcmaster.ca>
2023-12-08 01:27:50 -0500

    day 8 solutions

--- 2023_python_solutions/Day8/day8_P1.py
+++ 2023_python_solutions/Day8/day8_P1.py
@@ -5,6 +5,27 @@ import os
 
 #Input text not modified
 here = os.path.dirname(os.path.abspath(__file__))
-with open(here + "\\text7.txt") as file:
+with open(here + "\\text8.txt") as file:
     lines = file.read().splitlines()
 
+instruc = lines[0]
+mappings = {}
+
+for line in lines[1:]:
+    key, LR = line.split(" = ")
+    L, R = LR.split()
+    mappings[key] = (L,R)
+
+start = "AAA"
+moves = 0
+
+while True:
+    c = instruc[moves%len(instruc)]
+    if c == "R":
+        start = mappings[start][1]
+    else:# c == "L":
+        start = mappings[start][0]
+    moves += 1
+    if start == "ZZZ":
+        print(moves)
+        break
--- 2023_python_solutions/Day8/day8_P2.py
+++ 2023_python_solutions/Day8/day8_P2.py
@@ -1,10 +1,41 @@
-#Advent of Code 2023: Day 8 Part 2 solution
+#Advent of Code 2023: Day 8 Part 1 solution
 #Author: Pranay Kotian
 
 import os
 
 #Input text not modified
 here = os.path.dirname(os.path.abspath(__file__))
-with open(here + "\\text7.txt") as file:
+with open(here + "\\text8.txt") as file:
     lines = file.read().splitlines()
 
+instruc = lines[0]
+mappings = {}
+starts = []
+ends = []
+
+for line in lines[1:]:
+    key, LR = line.split(" = ")
+    L, R = LR.split()
+    mappings[key] = (L,R)
+    if key[2] == "A":
+        starts.append(key)
+    if key[2] == "Z":
+        ends.append(key)
+
+moves = 0
+minmoves = []
+
+for idx, start in enumerate(starts):
+    moves = 0
+    while True:
+        c = instruc[moves%len(instruc)]
+        if c == "R":
+            starts[idx] = mappings[starts[idx]][1]
+        else:# c == "L":
+            starts[idx] = mappings[starts[idx]][0]
+        moves += 1
+        if starts[idx][2] == "Z":
+            minmoves.append(moves)
+            break
+
+print(minmoves)
